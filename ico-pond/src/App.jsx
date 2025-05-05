f5609461fede17c78ed4b806c30f8e933fcfef2e
RafiRaihanFirdaus <hahanse>
2025-05-05 20:47:17 +0700

    add navbar to website

--- ico-pond/src/App.jsx
+++ ico-pond/src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes , Route } from "react-router-dom";
 import HomePage from "./pages/homepage";
+import NavbarComponent from "./components/Navbar";
 
 function App() {
   return (
       <div>
+        <NavbarComponent/>
         <Routes>
           <Route path="" element={<HomePage/>} />
         </Routes>
---
+++ ico-pond/src/label/index.jsx
@@ -0,0 +1,17 @@
+export const navLinks = [
+    {
+      id: 1,
+      path: "",
+      text: "Home",
+    },
+    {
+      id: 2,
+      path: "About",
+      text: "About",
+    },
+    {
+      id: 3,
+      path: "Monitoring",
+      text: "Product",
+    },  
+  ];
